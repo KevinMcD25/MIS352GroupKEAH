dde4d4139bdeb4b1dc2e144fa45fe14a7a601696
HaydenRhodes <hr00015@mix.wvu.edu>
2024-10-03 14:28:25 -0400

    pulling db

--- MIS353_ApplicationGroupKEAH/MIS353_ApplicationGroupKEAH/obj/Debug/net8.0/MIS353_ApplicationGroupKEAH.AssemblyInfo.cs
+++ MIS353_ApplicationGroupKEAH/MIS353_ApplicationGroupKEAH/obj/Debug/net8.0/MIS353_ApplicationGroupKEAH.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MIS353_ApplicationGroupKEAH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78480bdbf3323a03f8c063285527fbf06acf1897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb91b3ce99c94fbed714e9b17fcde896fb09b751")]
 [assembly: System.Reflection.AssemblyProductAttribute("MIS353_ApplicationGroupKEAH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MIS353_ApplicationGroupKEAH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
