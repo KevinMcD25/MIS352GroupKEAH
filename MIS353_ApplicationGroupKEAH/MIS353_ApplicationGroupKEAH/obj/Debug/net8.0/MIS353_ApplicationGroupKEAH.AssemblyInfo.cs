319db3729bed46380c7068ab8264085fad2e752d
KevinMcD25 <147118038+KevinMcD25@users.noreply.github.com>
2024-10-30 11:09:04 -0400

    idk

--- MIS353_ApplicationGroupKEAH/MIS353_ApplicationGroupKEAH/obj/Debug/net8.0/MIS353_ApplicationGroupKEAH.AssemblyInfo.cs
+++ MIS353_ApplicationGroupKEAH/MIS353_ApplicationGroupKEAH/obj/Debug/net8.0/MIS353_ApplicationGroupKEAH.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MIS353_ApplicationGroupKEAH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a17d4f442bd1aaa882273415943b8e8c9e44d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6887419704e216014c6e2730656eaba0192b1edd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MIS353_ApplicationGroupKEAH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MIS353_ApplicationGroupKEAH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
