5deff0cbe2467d68c00818939e3513438b756ac8
KevinMcD25 <147118038+KevinMcD25@users.noreply.github.com>
2024-10-31 13:55:36 -0400

    Idk

--- MIS353_ApplicationGroupKEAH/AdventureWVApi/Repositories/IUserTravelService.cs
+++ MIS353_ApplicationGroupKEAH/AdventureWVApi/Repositories/IUserTravelService.cs
@@ -1,6 +1,10 @@
-﻿namespace AdventureWVApi.Repositories
+﻿using AdventureWVApi.Data;
+
+namespace AdventureWVApi.Repositories
 {
     public interface IUserTravelService
     {
+        Task<int> UserTravelDelete(int UTID);
+        Task<int> AddUserTravel(int Pid, int Uid, DateTime UTDateTime);
     }
 }
