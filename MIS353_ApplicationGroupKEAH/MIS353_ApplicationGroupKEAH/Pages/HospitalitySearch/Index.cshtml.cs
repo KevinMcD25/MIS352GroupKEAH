898850941534523393aaccb412a33f9e73c6283a
HaydenRhodes <hr00015@mix.wvu.edu>
2024-11-14 09:52:26 -0500

    before we nuke this project

---
+++ MIS353_ApplicationGroupKEAH/MIS353_ApplicationGroupKEAH/Pages/HospitalitySearch/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MIS353_ApplicationGroupKEAH.Data;
+using System.Net.Http;
+
+namespace MIS353_ApplicationGroupKEAH.Pages.HospitalitySearch
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+
+        public IndexModel(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IList<Hospitality> Hospitality { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string Htype)
+        {
+            if (string.IsNullOrEmpty(Htype))
+            {
+                return Page();
+            }
+            try
+            {
+                //Made the endpoint string
+                string requestUrl = $"https://localhost:7151/api/Hospitality/SearchHType?HType={Uri.EscapeDataString(Htype)}";
+                //Send GET Request to API
+
+                Hospitality = await _httpClient.GetFromJsonAsync<IList<Hospitality>>(requestUrl) ?? new List<Hospitality>();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return Page();
+        }
+    }
+}
+
