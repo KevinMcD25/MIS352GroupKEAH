0e2691b199e725b0179621bc66f01735c36bef49
HaydenRhodes <hr00015@mix.wvu.edu>
2024-12-04 21:14:52 -0500

    added Pages to WV2.1

---
+++ MIS353_ApplicationGroupKEAH/AdventureWV2.1/Pages/Information.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdventureWV2._1.Pages
+{
+    public class InformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
